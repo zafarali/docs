68a27c990bac21ee82020764ef453f825e7c3564
Calem Bendell <calem.j.bendell@gmail.com>
2014-05-03 22:52:07 -0400

    push research summary and icon changes!

---
+++ media/logo/hyvedev-logo.tf
@@ -0,0 +1,17 @@
+\relax
+\ProvidesFile{hyvedev-logo.tf}[<typefacepackage auxiliary file>]
+\newcommand*\tf@aux@fontencoding{T1}
+\newcommand*\tf@aux@inputencoding{utf8}
+\newcommand*\tf@aux@textcomp{full}
+\newcommand*\tf@aux@cmap{resetfonts}
+\newcommand*\tf@aux@rmfontname{default}
+\newcommand*\tf@aux@sffontname{default}
+\newcommand*\tf@aux@ttfontname{default}
+\newcommand*\tf@aux@mathfontname{default}
+\newcommand*\tf@aux@symbolsfontname{default}
+\newcommand*\tf@aux@textfiguresfontname{default}
+\newcommand*\tf@aux@sfscalefactor{0.8507}
+\newcommand*\tf@aux@ttscalefactor{1}
+\newcommand*\tf@aux@mathscalefactor{0.99976}
+\newcommand*\tf@aux@textfiguresscalefactor{1}
+\newcommand*\tf@aux@textfiguresspacing{0}
